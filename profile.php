2b176fb77823bd40861609eb24239d7e28a3ce79
sanfo3855 <sanfelicimatteo@gmail.com>
2017-03-03 10:20:39 +0100

    Add files via upload

--- profile.php
+++ profile.php
@@ -25,7 +25,10 @@
                 font-size: 12px;
                 letter-spacing: 3px;
             }
-
+            .bg-1 { 
+                background-color: #b52c27;
+                color: #cccccc;
+            }
             .bg-3 { 
                 background-color: #cccccc; /* White */
                 color: #b52c27;
@@ -34,6 +37,10 @@
             .navbar-nav  li a:hover {
                 color: #1abc9c !important;
             }
+            .container-fluid {
+                padding-top: 70px;
+                padding-bottom: 50px;
+            }
         </style>
         <?php
         //starting session to store data usable in multiple pages
@@ -41,7 +48,6 @@
         ?>
     </head>
     <body>
-
         <nav class="navbar navbar-default">
             <div class="container">
                 <div class="navbar-header">
@@ -52,6 +58,7 @@
                     </button>
                     <a class="navbar-brand" style="font-size: 250%" href="home.php?register=<?php echo $_GET["register"]; ?>">Unibo Social Sport Sharing</a>
                 </div>
+
                 <div class="collapse navbar-collapse" id="myNavbar">
                     <ul class="nav navbar-nav navbar-right">
                         <?php
@@ -61,7 +68,7 @@
                             <?php
                         }
                         ?>
-                        <li><a href="impianti.php?register=<?php echo $_GET["register"]."&lat=44.4937143&long=11.3428948";?>">IMPIANTI</a></li>
+                        <li><a href="impianti.php?register=<?php echo $_GET["register"]."&lat=44.4937143&long=11.3428948"; ?>">IMPIANTI</a></li>
                         <?php
                         if ($_SESSION["username"] != "") {
                             ?>
@@ -77,10 +84,9 @@
         </nav>
 
 
-        <div class="container-fluid text-center"style=" padding-top: 20px; background-color: #b52c27; padding-left: 30px;padding-right: 30px">    
+        <div class="container-fluid bg-1 text-center"style=" padding-left: 30px; padding-right: 30px">    
             <div class="row">
-                <div class="col-sm-2 well"style=" background-color: #cccccc; border-color: #cccccc">
-                    <div 
+                <div class="col-md-3 well"style=" background-color: #cccccc; border-color: #ccccccs">
                     <?php
                     require 'connection.php';
 
@@ -91,22 +97,23 @@
                     } catch (Exception $ex) {
                         echo $ex;
                     }
-
                     $fotoUtente = $result->fetch();
-                    ?>  </div>
+                    ?>
+
                     <p><h4 style=" color: #b52c27;"><?php echo $_SESSION["nome_utente"] . " " . $_SESSION["cognome_utente"] ?></h4></p>
+
                     <a href="#aboutModal" data-toggle="modal" data-target="#myModal"><img src="<?php echo $fotoUtente["Foto"] ?>" class="img-circle" height="120" width="120" alt="Avatar">
-                        </div>
+
                         <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                             <div class="modal-dialog">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h4 class="title">Profile</h4>
+                                        <h4 class="title"style=" color: #b52c27">Profilo</h4>
                                     </div>
                                     <div class="modal-body">
                                         <center>
                                             <img src="<?php echo $fotoUtente["Foto"] ?>" name="aboutme" width="140" height="140" border="0" class="img-circle"></a>
-                                            <h3 class="media-heading" style=" color: #2f2f2f"><?php echo $_SESSION["nome_utente"] . " " . $_SESSION["cognome_utente"] ?><br><small><?php echo $_SESSION["nome_corso_studi"] ?></small>
+                                            <h3 class="media-heading"style=" color: #b52c27"><?php echo $_SESSION["nome_utente"] . " " . $_SESSION["cognome_utente"] ?><br><small><?php echo $_SESSION["nome_corso_studi"] ?></small>
                                                 <br><small>N° Matricola: <?php echo $_SESSION["num_matricola"] ?></small></h3>
                                         </center>
                                         <hr>
@@ -126,60 +133,63 @@
                                 </div>
                             </div>
                         </div>
-
-                        <p>
-                        <h5><small><i>Click per informazioni</i></small></h5>
-                        </p>
+                        <p><h5><small><i>Clicca la foto per le informazioni personali</i></small></h5></p>
+                        <br><br><br><br>
+                        <?php if ($_SESSION["tipo_utente"] == "UP") {
+                            ?>
+                                <button type="button" class="btn btn-primary btn-lg"style="   background-color: #b52c27; border-color: #b52c27">Crea Partita</button>
+                                    <?php
+                                }
+                                ?>
+                              
                 </div>
-            </div>     
-        </div>
-    </div>
-</div>
-
-<div class="container-fluid bg-3 text-center" style=" padding-top: 20px">
-    <div>
-        <?php
-        require 'connection.php';
-
-        //Selection of Regolamento and Foto
-        try {
-            $querySelectPallavolo = "SELECT Regolamento, Foto FROM SPORT";
-            $result = $conn->query($querySelectPallavolo);
-        } catch (Exception $ex) {
-            echo $ex;
-        }
-        $i = 0;
-        while ($row = $result->fetch()) {
-            $regolamenti[$i] = $row['Regolamento'];
-            $foto[$i] = $row['Foto'];
-            $i++;
-        }
-        ?>  </div>  
-    <!--<h2 class="margin">Trova il tuo sport</h2><br>-->
-    <div class="row">
-        <div class="col-sm-4">
-            <a href="<?php echo $regolamenti[1] ?>" target="_blank" style="color:#990000" >
-
-                <h3>Scopri la pallavolo</h3>
-                <img src="<?php echo $foto[1] ?>" class="img-responsive margin" style="display:inLine" alt="volleyRules" width="200" height="200">
-            </a>
+            </div>
         </div>
 
-        <div class="col-sm-4"> 
-            <a href="<?php echo $regolamenti[0] ?>" target="_blank" style="color:#990000" >
-                <h3>Scopri il calcio</h3>
-                <img src="<?php echo $foto[0] ?>" class="img-responsive margin" style="display:inLine" alt="calcioRules" width="210" height="210">
-            </a>
-        </div>
-        <div class="col-sm-4"> 
-            <a href="<?php echo $regolamenti[2] ?>" target="_blank" style="color:#990000" >
-                <h3>Scopri il tennis</h3>
-                <img src="<?php echo $foto[2] ?>" class="img-responsive margin" style="display:inLine" alt="tennisRules" width="190" height="190">
-            </a>
-        </div>
-    </div>
-</div>
+        <div class="container-fluid bg-3 text-center">
+            <div>
+                <?php
+                require 'connection.php';
 
-</body>
+                //Selection of Regolamento and Foto
+                try {
+                    $querySelectPallavolo = "SELECT Regolamento, Foto FROM SPORT";
+                    $result = $conn->query($querySelectPallavolo);
+                } catch (Exception $ex) {
+                    echo $ex;
+                }
+                $i = 0;
+                while ($row = $result->fetch()) {
+                    $regolamenti[$i] = $row['Regolamento'];
+                    $foto[$i] = $row['Foto'];
+                    $i++;
+                }
+                ?>  </div>  
+            <!--<h2 class="margin">Trova il tuo sport</h2><br>-->
+            <div class="row">
+                <div class="col-sm-4">
+                    <a href="<?php echo $regolamenti[1] ?>" target="_blank" style="color:#990000" >
+
+                        <h3>Scopri la pallavolo</h3>
+                        <img src="<?php echo $foto[1] ?>" class="img-responsive margin" style="display:inLine" alt="volleyRules" width="200" height="200">
+                    </a>
+                </div>
+
+                <div class="col-sm-4"> 
+                    <a href="<?php echo $regolamenti[0] ?>" target="_blank" style="color:#990000" >
+                        <h3>Scopri il calcio</h3>
+                        <img src="<?php echo $foto[0] ?>" class="img-responsive margin" style="display:inLine" alt="calcioRules" width="210" height="210">
+                    </a>
+                </div>
+                <div class="col-sm-4"> 
+                    <a href="<?php echo $regolamenti[2] ?>" target="_blank" style="color:#990000" >
+                        <h3>Scopri il tennis</h3>
+                        <img src="<?php echo $foto[2] ?>" class="img-responsive margin" style="display:inLine" alt="tennisRules" width="190" height="190">
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+    </body>
 </html>
 
